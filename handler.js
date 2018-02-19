1a2747d1fb2e90bfe97b4da34846667b8e9b0368
Remiii <repository@remibarbe.fr>
2018-02-19 16:07:03 +0100

    first commit

---
+++ handler.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports.main = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+    },
+    body: JSON.stringify({
+      message: 'Hello world!',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+};
+
